ce71a5f0ff191612d6148b6480866be086785ec5
lorewar2 <mw8511@live.com>
2024-08-08 01:43:23 -0500

    display small section of cluster centers

--- src/main.rs
+++ src/main.rs
@@ -680,11 +680,13 @@ fn test_long_switch(start_index: usize, end_index: usize,
             .fetch(chrom, vcf_fetch_start, vcf_fetch_end) // TODO dont hard code things
             .expect("could not fetch in vcf");
         let (molecules, first_var_index, last_var_index) = get_read_molecules(vcf_reader, &vcf_info, READ_TYPE::HIFI, data.ploidy);
+        let mut cluster_centers_for_display = vec![];
         for (index, pairing) in pairings.iter().enumerate() {
             // TODO just deleted this, but might need it back
             //swap(cluster_centers, breakpoint, &pairing, 50);
             //swap_copied(&mut cluster_center_copies[index], breakpoint, &pairing);
-            let new_cluster_centers = swap_and_return_fixed(&cluster_centers, &cluster_center_copies[index], breakpoint);
+            let (new_cluster_centers, small_cluster) = swap_and_return_fixed(&cluster_centers, &cluster_center_copies[index], breakpoint);
+            cluster_centers_for_display.push(small_cluster);
             let mut posteriors: Vec<Vec<f32>> = Vec::new();
             for moldex in molecules.iter() {
                 let mut post: Vec<f32> = Vec::new();
@@ -709,14 +711,14 @@ fn test_long_switch(start_index: usize, end_index: usize,
                 println!("posterior for pair {} VVV: {}", ll_index, posterior);
                 // print the pairing
                 if data.ploidy == 4 {
-                    println!("{} - {}", pairings[ll_index][0].0, pairings[ll_index][0].1);
-                    println!("{} - {}", pairings[ll_index][1].0, pairings[ll_index][1].1);
-                    println!("{} - {}", pairings[ll_index][2].0, pairings[ll_index][2].1);
-                    println!("{} - {}", pairings[ll_index][3].0, pairings[ll_index][3].1);
+                    println!("{} - {} {:?}", pairings[ll_index][0].0, pairings[ll_index][0].1, cluster_centers_for_display[ll_index][0]);
+                    println!("{} - {} {:?}", pairings[ll_index][1].0, pairings[ll_index][1].1, cluster_centers_for_display[ll_index][1]);
+                    println!("{} - {} {:?}", pairings[ll_index][2].0, pairings[ll_index][2].1, cluster_centers_for_display[ll_index][2]);
+                    println!("{} - {} {:?}", pairings[ll_index][3].0, pairings[ll_index][3].1, cluster_centers_for_display[ll_index][3]);
                 }
                 else if data.ploidy == 2 {
-                    println!("{} - {}", pairings[ll_index][0].0, pairings[ll_index][0].1);
-                    println!("{} - {}", pairings[ll_index][1].0, pairings[ll_index][1].1);
+                    println!("{} - {} {:?}", pairings[ll_index][0].0, pairings[ll_index][0].1, cluster_centers_for_display[ll_index][0]);
+                    println!("{} - {} {:?}", pairings[ll_index][1].0, pairings[ll_index][1].1, cluster_centers_for_display[ll_index][1]);
                 }
             }
             if posterior > best_post {
@@ -801,7 +803,7 @@ fn test_long_switch(start_index: usize, end_index: usize,
     to_return
 }
 
-fn swap_and_return_fixed(cluster_centers: &Vec<Vec<f32>>, cluster_centers_copy: &Vec<Vec<f32>>, breakpoint: usize) -> Vec<Vec<f32>> {
+fn swap_and_return_fixed(cluster_centers: &Vec<Vec<f32>>, cluster_centers_copy: &Vec<Vec<f32>>, breakpoint: usize) -> (Vec<Vec<f32>>, Vec<Vec<f32>>){
     let mut cluster_centers_new = cluster_centers.clone();
     // until breakpoint normal after breakpoint swapped
     for i in breakpoint..cluster_centers[0].len() {
@@ -809,7 +811,15 @@ fn swap_and_return_fixed(cluster_centers: &Vec<Vec<f32>>, cluster_centers_copy: 
             cluster_centers_new[hap][i] = cluster_centers_copy[hap][i]
         }
     }
-    return cluster_centers_new;
+    let mut cluster_centers_small = vec![];
+    for hap in 0..cluster_centers.len() {
+        let mut one_hap = vec![];
+        for i in breakpoint - 5.. breakpoint + 5 {
+            one_hap.push(cluster_centers_new[hap][i]);
+        }
+        cluster_centers_small.push(one_hap);
+    }
+    return (cluster_centers_new, cluster_centers_small);
 }
 
 fn swap_copied(cluster_centers_copy: &mut Vec<Vec<f32>>, breakpoint: usize, pairing: &Vec<(usize, usize)>) {
